7fa7f29c9c2c0a81d34d434da92da049d03b370e
Ketut <adisuprianta13@gmail.com>
2020-10-11 17:21:49 +0700

    selesai katalog

--- resources/views/jersey.blade.php
+++ resources/views/jersey.blade.php
@@ -9,31 +9,119 @@
         
     </div>
     <div class="container">
+        <div class="row">
+            <div class="col-6">
+                    <img src="{{asset('assets/images/examples/image1.png')}}" height="400px"> 
+            </div>
+        </div>
         <div class="row content">
             <div class="col-md-10 offset-md-1">
-                <div class="row">    
+                <div class="row content-isi">    
                     <div class="col-4">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{asset('assets/images/examples/product-4.jpg')}}">
-                            <div class="card-body">Content</div>
-                            <div class="card-footer">Footer</div>
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket A
+                                </h5></center>
+                                <p class="card-text">
+                                    Full set, atas full printing, nomor celana polyflex/sablon/bordir
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >135.000</p>
+                            </div>  
                         </div>
                     </div>
+                    
                     <div class="col-4">
-                    <div class="card bg-primary text-white " style="width: 18rem;">
-                            <img src="{{asset('assets/images/examples/product-4.jpg')}}">
-                            <div class="card-body">Content</div>
-                            <div class="card-footer">Footer</div>
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket B
+                                </h5></center>
+                                <p class="card-text">
+                                    Full set, printing depan belakang, lengan kain, nomor celana polyflex/sablon/bordir
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >127.500</p>
+                            </div>  
                         </div>
                     </div>
+
                     <div class="col-4">
-                    <div class="card bg-primary text-white" style="width: 18rem;">
-                            <img src="{{asset('assets/images/examples/product-4.jpg')}}">
-                            <div class="card-body">Content</div>
-                            <div class="card-footer">Footer</div>
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket C
+                                </h5></center>
+                                <p class="card-text">
+                                    Printing depan saja, nama dan no punggung sablon poliflex/sablon, nomor celana sablon poliflex/sablon
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >127.500</p>
+                            </div>  
                         </div>
                     </div>
                 </div>
+                
+                <div class="row content-isi">    
+                    <div class="col-4">
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket A
+                                </h5></center>
+                                <p class="card-text">
+                                    Full set, atas full printing, nomor celana polyflex/sablon/bordir
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >135.000</p>
+                            </div>  
+                        </div>
+                    </div>
+                    
+                    <div class="col-4">
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket B
+                                </h5></center>
+                                <p class="card-text">
+                                    Full set, printing depan belakang, lengan kain, nomor celana polyflex/sablon/bordir
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >127.500</p>
+                            </div>  
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="card bg-jersey text-dark" style="width: 16rem;">
+                            <img src="{{asset('assets/images/examples/A.jpeg')}}">
+                            <hr>
+                            <div class="card-body">
+                                <center><h5 class="card-title">
+                                    Paket C
+                                </h5></center>
+                                <p class="card-text">
+                                    Printing depan saja, nama dan no punggung sablon poliflex/sablon, nomor celana sablon poliflex/sablon
+                                </p>
+                                <a href="#" class="btn btn-primary btn-card">Beli</a>
+                                <p class="float-right" >127.500</p>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
